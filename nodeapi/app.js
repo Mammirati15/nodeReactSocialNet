640d16fb49b8349994ce446508234b5506b6a9ed
Matt Ammirati <matt.ammirati@gmail.com>
2020-11-11 17:04:57 -1000

    connected to mongoDB

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+
+//db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+.then(() => {console.log("DB Connected")})
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+})
 
 //bring routes
 const postRoutes = require("./routes/post")
@@ -12,5 +24,6 @@ app.use(morgan("dev"))
 
 app.use("/", postRoutes)
 
-const port = 8080
+const port = process.env.PORT || 8080;
+
 app.listen(port, () => {console.log(`A Node Js api is listening on port: ${port}`)})
