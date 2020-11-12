448f0adfe46685138bbc2bd2012011267fe702f5
Matt Ammirati <matt.ammirati@gmail.com>
2020-11-11 17:47:29 -1000

    added a schema and connected to database

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+
 
 dotenv.config()
 
@@ -20,7 +22,7 @@ const postRoutes = require("./routes/post")
 
 //middleware
 app.use(morgan("dev"))
-
+app.use(bodyParser.json())
 
 app.use("/", postRoutes)
 
