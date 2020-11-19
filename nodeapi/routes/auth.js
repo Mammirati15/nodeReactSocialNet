9807dd43aab0a7a17a30fabf8a1719d57cb7cfef
Matt Ammirati <matt.ammirati@gmail.com>
2020-11-19 11:18:18 -1000

    added the user info into a request body

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { signUp, signin, signout } = require('../controllers/auth')
-
+const { userById } = require('../controllers/user')
 const { userSignupValidator } = require('../validator/index')
 
 
@@ -10,6 +10,9 @@ const router = express.Router()
 router.post("/signup", userSignupValidator, signUp);
 router.post("/signin", signin);
 router.get("/signout", signout)
+
+// if route contains userId the app will run userById(Id)
+router.param('userId', userById)
 
 module.exports = router;
 
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {getPosts, createPost} = require('../controllers/post')
 const { requireSignin } = require('../controllers/auth')
+const { userById } = require('../controllers/user')
 const { createPostValidator } = require('../validator/index')
 
 
@@ -8,6 +9,9 @@ const router = express.Router()
 
 router.get('/', requireSignin, getPosts);
 router.post('/post', createPostValidator, createPost);
+
+// if route contains userId the app will run userById(Id)
+router.param('userId', userById)
 
 module.exports = router;
 
