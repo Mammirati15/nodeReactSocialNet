2ac32e9c61398ca26e1d8b618d57b3c6b82af133
Matt Ammirati <matt.ammirati@gmail.com>
2020-11-21 11:10:53 -1000

    set up update user with ID

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { userById, allUsers, getUser } = require('../controllers/user')
+const { userById, allUsers, getUser, updateUser } = require('../controllers/user')
 const { requireSignin } = require('../controllers/auth')
 
 
@@ -9,6 +9,7 @@ const router = express.Router()
 
 router.get("/users", allUsers)
 router.get("/user/:userId", requireSignin, getUser)
+router.put("/user/:userId", requireSignin, updateUser)
 
 // if route contains userId the app will run userById(Id)
 router.param('userId', userById)
