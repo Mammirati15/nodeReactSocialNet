04be3c6d43b14c63691d4e160a78bf3467b692a8
Matt Ammirati <matt.ammirati@gmail.com>
2020-11-26 12:53:18 -1000

    auth id is not matching postedby id, maybe have to login as different user

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {getPosts, createPost, postsByUser} = require('../controllers/post')
+const {getPosts, createPost, postsByUser, postById, isPoster, deletePost} = require('../controllers/post')
 const { requireSignin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
 const { createPostValidator } = require('../validator/index')
@@ -9,9 +9,14 @@ const router = express.Router()
 
 router.get('/', getPosts);
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
-router.get("/posts/by/:userId", requireSignin, postsByUser)
+router.get("/posts/by/:userId", postsByUser)
+router.delete("/post/:postId", requireSignin, isPoster, deletePost)
+
 // if route contains userId the app will run userById(Id)
 router.param('userId', userById)
+
+// if route contains userId the app will run postById(Id)
+router.param('postId', postById)
 
 module.exports = router;
 
