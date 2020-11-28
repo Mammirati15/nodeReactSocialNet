a1267bc9adf8d13bb17cacca3e3895ef604589bc
Matt Ammirati <matt.ammirati@gmail.com>
2020-11-28 13:42:58 -1000

    created update post function, still getting unauthorized error

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {getPosts, createPost, postsByUser, postById, isPoster, deletePost} = require('../controllers/post')
+const {getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost} = require('../controllers/post')
 const { requireSignin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
 const { createPostValidator } = require('../validator/index')
@@ -10,6 +10,7 @@ const router = express.Router()
 router.get('/', getPosts);
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
 router.get("/posts/by/:userId", postsByUser)
+router.put("/post/:postId", requireSignin, isPoster, updatePost)
 router.delete("/post/:postId", requireSignin, isPoster, deletePost)
 
 // if route contains userId the app will run userById(Id)
