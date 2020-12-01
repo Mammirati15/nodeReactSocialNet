9b52b3a5f894d98f026cf82ddb5ddc0fa84c1696
Matt Ammirati <matt.ammirati@gmail.com>
2020-11-30 16:43:09 -1000

    set up react

---
+++ react-social-net/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>React Front End</h1>
+    </div>
+  );
+}
+
+export default App;
