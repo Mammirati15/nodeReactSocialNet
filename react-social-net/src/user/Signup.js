7cd8e9a3a1fe7257ba56c2f778288112dc5b6942
Matt Ammirati <matt.ammirati@gmail.com>
2020-12-01 23:51:44 -1000

    created signup page

--- react-social-net/src/user/Signup.js
+++ react-social-net/src/user/Signup.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 
 class Signup extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name: "", 
+      email: "",
+      password: "",
+      error: ""
+    }
+  }
+  
   render(){
     return (
-      <div>
-        <h2>Signup screen</h2>
+      <div className="container">
+        <h2 className="mt-5 mb-5">Signup For The Social Network</h2>
+        <form>
+          <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input type="email" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input type="password" className="form-control" />
+          </div>
+          <button className="mt-2 btn btn-raised btn-primary">Signup</button>
+        </form>
       </div>
     )
   }
