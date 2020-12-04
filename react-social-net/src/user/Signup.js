d5d79482081a3e34453962703b35e0b2265756b5
Matt Ammirati <matt.ammirati@gmail.com>
2020-12-03 14:17:35 -1000

    made backend call and created new user, saved to mongo

--- react-social-net/src/user/Signup.js
+++ react-social-net/src/user/Signup.js
@@ -14,25 +14,49 @@ class Signup extends Component {
   handleChange = (name) => (event) => {
     this.setState({[name]: event.target.value})
   }
+
+  clickSubmit = event => {
+    event.preventDefault()
+    const {name, email, password} = this.state
+    const user = {
+      name: name,
+      email: email,
+      password: password
+    }
+    //console.log(user)
+    fetch("http://localhost:8080/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+    .then(response => {
+      return response.json()
+    })
+    .catch(err => console.log(err))
+  }
   
   render(){
+    const {name, email, password} = this.state
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">Signup For The Social Network</h2>
         <form>
           <div className="form-group">
             <label className="text-muted">Name</label>
-            <input onChange={this.handleChange("name")} type="text" className="form-control" />
+            <input onChange={this.handleChange("name")} type="text" className="form-control" value={name} />
           </div>
           <div className="form-group">
             <label className="text-muted">Email</label>
-            <input onChange={this.handleChange("email")} type="email" className="form-control" />
+            <input onChange={this.handleChange("email")} type="email" className="form-control" value={email} />
           </div>
           <div className="form-group">
             <label className="text-muted">Password</label>
-            <input onChange={this.handleChange("password")} type="password" className="form-control" />
+            <input onChange={this.handleChange("password")} type="password" className="form-control" value={password} />
           </div>
-          <button className="mt-2 btn btn-raised btn-primary">Signup</button>
+          <button onClick={this.clickSubmit} className="mt-2 btn btn-raised btn-primary">Signup</button>
         </form>
       </div>
     )
