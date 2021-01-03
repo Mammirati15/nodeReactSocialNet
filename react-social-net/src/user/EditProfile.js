67bdcd422673f16c9769a557a581371cb7d22e10
Matt Ammirati <matt.ammirati@gmail.com>
2021-01-03 11:59:20 -1000

    made PUT request to backend to update the users profile through the edit profile section

--- react-social-net/src/user/EditProfile.js
+++ react-social-net/src/user/EditProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {isAuthenticated} from '../auth/index'
-import {read} from "./apiUser"
+import {read, update} from "./apiUser"
+import {Redirect} from 'react-router-dom'
 
 class EditProfile extends Component {
   constructor(){
@@ -9,7 +10,9 @@ class EditProfile extends Component {
       id: "",
       name: "",
       email: "",
-      password: ""
+      password: "",
+      redirectToProfile: false,
+      error: ""
     }
   }
 
@@ -18,9 +21,9 @@ class EditProfile extends Component {
     read(userId, token)
      .then(data => {
        if(data.error){
-         this.setState({redirectToSignin: true})
+         this.setState({redirectToProfile: true})
        } else {
-         this.setState({id: data._id, name: data.name, email: data.email})
+         this.setState({id: data._id, name: data.name, email: data.email, error: ''})
        }
      })
    }
@@ -40,20 +43,18 @@ class EditProfile extends Component {
     const user = {
       name: name,
       email: email,
-      password: password
+      password: password || undefined
     }
-    console.log(user)
-    // signup(user)
-    // .then(data => {
-    //   if(data.error) this.setState({error: data.error})
-    //     else this.setState({
-    //       error: "",
-    //       name: "",
-    //       email: "",
-    //       password: "",
-    //       open: true
-    //     })
-    // })
+    //console.log(user)
+    const userId = this.props.match.params.userId
+    const token = isAuthenticated().token 
+    update(userId, token, user)
+    .then(data => {
+      if(data.error) this.setState({error: data.error})
+        else this.setState({
+          redirectToProfile: true
+        })
+    })
   }
 
   signUpForm = (name, email, password) => (
@@ -75,7 +76,10 @@ class EditProfile extends Component {
   )
 
   render() {
-    const {name, email, password} = this.state
+    const {id, name, email, password, redirectToProfile} = this.state
+    if(redirectToProfile) {
+      return <Redirect to={`/user/${id}`} />
+    }
     return (
       <div className="container">
         <h2 className="mt-5 mb-5 ml-5">Edit Profile</h2>
