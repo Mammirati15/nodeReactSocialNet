7518b8f9f25503facc4f92e2c7aa62acba92032e
Matt Ammirati <matt.ammirati@gmail.com>
2021-01-07 11:43:41 -1000

    implemented the add following and add follower method in the controller and arranged the route using put request

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -116,3 +116,32 @@ exports.deleteUser = (req, res) => {
     res.json({message: "User has been deleted successfully"})
   })
 }
+
+//follow and unfollow
+
+exports.addFollowing = (req, res, next) => {
+  User.findByIdAndUpdate(req.body.userId, {$push: {following: req.body.followId}}, (err, result) => {
+    if(err) {
+      return res.status(400).json({error: err})
+    }
+    next()
+  } 
+  )
+}
+
+exports.addFollower = (req, res) => {
+  User.findByIdAndUpdate(req.body.followId, {$push: {followers: req.body.userId}}, {new: true}
+  )
+  .populate('following', '_id name')
+  .populate('followers', '_id name')
+  .exec((err, result) => {
+    if(err) {
+      return res.status(400).json({
+        error: err
+      })
+    }
+    result.hashed_password = undefined
+    result.salt = undefined
+    res.json(result)
+  })
+}
