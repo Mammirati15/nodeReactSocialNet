ef6a7ba54c2ba265b230220fdb1cdf6c850b17c9
Matt Ammirati <matt.ammirati@gmail.com>
2021-01-11 14:34:17 -1000

    added follow and unfollow functionality storing the data in the back end

--- react-social-net/src/user/Profile.js
+++ react-social-net/src/user/Profile.js
@@ -12,7 +12,8 @@ class Profile extends Component {
     this.state = {
       user: {following: [], followers: []},
       redirectToSignin: false,
-      following: false
+      following: false,
+      error: ''
     }
   }
 
@@ -24,6 +25,20 @@ class Profile extends Component {
       return follower._id === jwt.user._id
     })
     return match
+  }
+
+  clickFollowButton = callApi => {
+    const userId = isAuthenticated().user._id
+    const token = isAuthenticated().token
+
+    callApi(userId, token, this.state.user._id)
+    .then(data => {
+      if(data.error){
+        this.setState({error: data.error})
+      } else {
+        this.setState({user: data, following: !this.state.following})
+      }
+    })
   }
   
 
@@ -84,7 +99,7 @@ class Profile extends Component {
                 <DeleteUser userId={user._id} />
               </div>
             ) : (
-            <FollowProfileButton following={this.state.following} />
+            <FollowProfileButton following={this.state.following} onButtonClick={this.clickFollowButton} />
             )}
           </div>
         </div>
