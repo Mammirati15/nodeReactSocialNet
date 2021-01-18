b644b85362be949c04e99124c7c4c2b3a84faa56
Matt Ammirati <matt.ammirati@gmail.com>
2021-01-18 12:24:09 -1000

    displayed the full single post with image

--- react-social-net/src/post/SinglePost.js
+++ react-social-net/src/post/SinglePost.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import {singlePost} from './apiPost'
+import DefaultPost from "../images/hanauma.jpg"
+import {Link} from 'react-router-dom'
+
 
 class SinglePost extends Component {
   state = {
@@ -17,11 +20,45 @@ class SinglePost extends Component {
     })
   }
 
+  renderPost = (post) => {
+    const posterId = post.postedBy ? `/user/${post.postedBy._id}` : ''
+    const posterName = post.postedBy ? post.postedBy.name : ''
+    return (    
+        <div className="card-body">
+          <img 
+            src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}  
+            alt={post.title} onError={i => i.target.src = `${DefaultPost}`} 
+            className="img-thumbnail mb-3" 
+            style={{height: '300px', width: '100%', objectFit: 'cover'
+          }}
+          />
+          
+          <p className="card-text">
+          {post.body}
+          </p>
+          <br />
+          <p className="font-italic mark"> 
+            Posted By{" "} 
+            <Link to={`${posterId}`}>
+              {posterName}{" "}
+            </Link>
+            on {new Date(post.created).toDateString()}
+          </p>
+          <Link to={`/`} className="btn btn-raised btn-primary btn-sm">
+            Back to Posts
+          </Link>
+        </div>
+      
+    )
+}
+
   render() {
+    const {post} = this.state
     return (
-      <div>
-        <h2>Single Post</h2>
-        {JSON.stringify(this.state.post)}
+      <div className="container">
+        <h2 className="display-2 mt-5 mb-5">{post.title}</h2>
+        {this.renderPost(post)}
+        
       </div>
     )
   }
